bcd7b375a3143bee6a9195996d658ac3aceef92c
changchangch <45754721+changchangch@users.noreply.github.com>
2019-05-01 01:18:38 +0800

    Delete test27.js

--- randjs/test27.js
+++
@@ -1,4 +0,0 @@
-var imgnumber = Math.floor(Math.random()*3);
-var imgurl = ['./小火車/縮小/16.jpg', './小火車/縮小/17.jpg', './小火車/縮小/18.jpg'];
-var imgurlind = ['./小火車html/AlishanForestRailway.html', './小火車html/AlishanForestRailway.html', './小火車html/Taoyuan.html']
-document.write('<a href="'+imgurlind[imgnumber]+'" target="_blank">'+'<img src="'+imgurl[imgnumber]+'">'+'</a>');
