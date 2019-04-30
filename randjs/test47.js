b4f491425f9c954796eb0026485e8cee6857b9ff
changchangch <45754721+changchangch@users.noreply.github.com>
2019-05-01 00:51:59 +0800

    Add files via upload

--- randjs/test47.js
+++ randjs/test47.js
@@ -1,4 +1,4 @@
 var imgnumber = Math.floor(Math.random()*3);
 var imgurl = ['./人造建築物/縮小/16.jpg', './人造建築物/縮小/17.jpg', './人造建築物/縮小/18.jpg'];
 var imgurlind = ['./人造建築物html/TainanBeimen.html', './人造建築物html/TaijiangNationalPark.html', './人造建築物html/TainanCoffee.html']
-document.write('<a href="'+imgurlind[imgnumber]+'" target="_blank">'+'<img src="'+imgurl[imgnumber]+'">'+'</a>');
+document.write('<a href="'+imgurlind[imgnumber]+'" target="_blank">'+'<img src="'+imgurl[imgnumber]+'" style="height: 30%; width: 30%;">'+'</a>');
