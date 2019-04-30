0b2a2ce0d37db9fdfd1df1aebb07da9e42f62e6a
changchangch <45754721+changchangch@users.noreply.github.com>
2019-05-01 01:22:03 +0800

    Delete test64.js

--- randjs/test64.js
+++
@@ -1,4 +0,0 @@
-var imgnumber = Math.floor(Math.random()*4);
-var imgurl = ['./海邊/縮小/31.jpg', './海邊/縮小/06.jpg','./海邊/縮小/07.jpg', './海邊/縮小/08.jpg'];
-var imgurlind = ['./海邊html/PenghuShanshuiBeach.html', './海邊html/Torik(Duli).html','./海邊html/TaitungJinzun.html', './海邊html/Fenniaolin.html']
-document.write('<a href="'+imgurlind[imgnumber]+'" target="_blank">'+'<img src="'+imgurl[imgnumber]+'">'+'</a>');
