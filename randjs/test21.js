0ffdac45cba4f98f95ce7edaf8d7658404a4bec2
changchangch <45754721+changchangch@users.noreply.github.com>
2019-05-01 01:18:02 +0800

    Delete test21.js

--- randjs/test21.js
+++
@@ -1,4 +0,0 @@
-var imgnumber = Math.floor(Math.random()*3);
-var imgurl = ['./小火車/縮小/01.jpg', './小火車/縮小/02.jpg', './小火車/縮小/03.jpg'];
-var imgurlind = ['./小火車html/TaipeiMetropolitanPark.html', './小火車html/TaipeiMetropolitanPark.html', './小火車html/NeiwanLine.html']
-document.write('<a href="'+imgurlind[imgnumber]+'" target="_blank">'+'<img src="'+imgurl[imgnumber]+'">'+'</a>');
