26932e2a7bb482ebeea34140b3c0d6067266b10d
changchangch <45754721+changchangch@users.noreply.github.com>
2019-05-01 01:19:13 +0800

    Delete test34.js

--- randjs/test34.js
+++
@@ -1,4 +0,0 @@
-var imgnumber = Math.floor(Math.random()*3);
-var imgurl = ['./花海/縮小/04.jpg', './花海/縮小/05.jpg', './花海/縮小/06.jpg'];
-var imgurlind = ['./花海html/Xinshe.html', './花海html/Xinshe.html', './花海html/Xinshe.html']
-document.write('<a href="'+imgurlind[imgnumber]+'" target="_blank">'+'<img src="'+imgurl[imgnumber]+'">'+'</a>');
