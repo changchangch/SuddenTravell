06f46dc497b2bf39e77e27ac8ba890d7f5858b39
changchangch <45754721+changchangch@users.noreply.github.com>
2019-05-01 01:18:21 +0800

    Delete test24.js

--- randjs/test24.js
+++
@@ -1,4 +0,0 @@
-var imgnumber = Math.floor(Math.random()*3);
-var imgurl = ['./小火車/縮小/04.jpg', './小火車/縮小/05.jpg', './小火車/縮小/06.jpg'];
-var imgurlind = ['./小火車html/NeiwanLine.html', './小火車html/NeiwanLine.html', './小火車html/TaipingshanBongBongTrain.html']
-document.write('<a href="'+imgurlind[imgnumber]+'" target="_blank">'+'<img src="'+imgurl[imgnumber]+'">'+'</a>');
