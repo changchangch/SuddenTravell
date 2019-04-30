e3c6bc9682892d9b3ea5b532f8f38e52ac105e46
changchangch <45754721+changchangch@users.noreply.github.com>
2019-05-01 01:21:57 +0800

    Delete test63.js

--- randjs/test63.js
+++
@@ -1,4 +0,0 @@
-var imgnumber = Math.floor(Math.random()*4);
-var imgurl = ['./海邊/縮小/26.jpg', './海邊/縮小/10.jpg', './海邊/縮小/28.jpg', './海邊/縮小/29.jpg'];
-var imgurlind = ['./海邊html/YinyangSea.html', './海邊html/Fenniaolin.html', './海邊html/HsinchuHaishanFishingPort.html', './海邊html/ChiayiDongshihFishingPort.html']
-document.write('<a href="'+imgurlind[imgnumber]+'" target="_blank">'+'<img src="'+imgurl[imgnumber]+'">'+'</a>');
