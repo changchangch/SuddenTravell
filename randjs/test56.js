aca38a95eb51d6569325a8e044113182d1d2bf7b
changchangch <45754721+changchangch@users.noreply.github.com>
2019-05-01 01:21:20 +0800

    Delete test56.js

--- randjs/test56.js
+++
@@ -1,4 +0,0 @@
-var imgnumber = Math.floor(Math.random()*3);
-var imgurl = ['./摩天輪/縮小/11.jpg', './摩天輪/縮小/14.jpg', './摩天輪/縮小/17.jpg'];
-var imgurlind = ['./摩天輪html/EDAWorld.html', './摩天輪html/TCAP.html', './摩天輪html/WindowonChina.html']
-document.write('<a href="'+imgurlind[imgnumber]+'" target="_blank">'+'<img src="'+imgurl[imgnumber]+'">'+'</a>');
