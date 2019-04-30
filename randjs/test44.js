11b2ffa5457ad28d1a8defa211adf23133fe0e11
changchangch <45754721+changchangch@users.noreply.github.com>
2019-05-01 01:20:14 +0800

    Delete test44.js

--- randjs/test44.js
+++
@@ -1,4 +0,0 @@
-var imgnumber = Math.floor(Math.random()*3);
-var imgurl = ['./人造建築物/縮小/04.jpg', './人造建築物/縮小/05.jpg', './人造建築物/縮小/06.jpg'];
-var imgurlind = ['./人造建築物html/taichungsea.html', './人造建築物html/NationalTaichungTheater.html', './人造建築物html/RainbowVillage.html']
-document.write('<a href="'+imgurlind[imgnumber]+'" target="_blank">'+'<img src="'+imgurl[imgnumber]+'" style="max-width: 95%; margin: auto;">'+'</a>');
