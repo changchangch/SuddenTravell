a64ea65205b38462db568ccf0cf8f47818ef90c1
changchangch <45754721+changchangch@users.noreply.github.com>
2019-05-01 01:04:19 +0800

    Add files via upload

--- randjs/test41.js
+++ randjs/test41.js
@@ -1,4 +1,4 @@
 var imgnumber = Math.floor(Math.random()*3);
 var imgurl = ['./人造建築物/縮小/01.jpg', './人造建築物/縮小/02.jpg', './人造建築物/縮小/03.jpg'];
 var imgurlind = ['./人造建築物html/LiloBaseBallCreativePark.html', './人造建築物html/LiloBaseBallCreativePark.html', './人造建築物html/taichung.html']
-document.write('<a href="'+imgurlind[imgnumber]+'" target="_blank">'+'<img src="'+imgurl[imgnumber]+'" style="height: 30%; width: 30%;">'+'</a>');
+document.write('<a href="'+imgurlind[imgnumber]+'" target="_blank">'+'<img src="'+imgurl[imgnumber]+'" style="max-width: 95%; margin: auto;">'+'</a>');
--- randjs/test42.js
+++ randjs/test42.js
@@ -1,4 +1,4 @@
 var imgnumber = Math.floor(Math.random()*3);
 var imgurl = ['./人造建築物/縮小/10.jpg', './人造建築物/縮小/11.jpg', './人造建築物/縮小/12.jpg'];
 var imgurlind = ['./人造建築物html/Taipei101.html', './人造建築物html/Taipei101.html', './人造建築物html/SongshanCulturalandCreativePark.html']
-document.write('<a href="'+imgurlind[imgnumber]+'" target="_blank">'+'<img src="'+imgurl[imgnumber]+'">'+'</a>');
+document.write('<a href="'+imgurlind[imgnumber]+'" target="_blank">'+'<img src="'+imgurl[imgnumber]+'" style="max-width: 95%; margin: auto;">'+'</a>');
