471c199ac59d8ae49f879e997e354ac126fd416c
changchangch <45754721+changchangch@users.noreply.github.com>
2019-05-01 01:18:48 +0800

    Delete test29.js

--- randjs/test29.js
+++
@@ -1,4 +0,0 @@
-var imgnumber = Math.floor(Math.random()*3);
-var imgurl = ['./小火車/縮小/25.jpg', './小火車/縮小/26.jpg', './小火車/縮小/27.jpg'];
-var imgurlind = ['./小火車html/PIER-2.html', './小火車html/HsinchuKangLang.html', './小火車html/OldMountainLineRailBike.html']
-document.write('<a href="'+imgurlind[imgnumber]+'" target="_blank">'+'<img src="'+imgurl[imgnumber]+'">'+'</a>');
