04edd3c19af670276c5c01912498aad28a8ff0ec
changchangch <45754721+changchangch@users.noreply.github.com>
2019-05-01 03:11:48 +0800

    Add files via upload

--- randjs/test63.js
+++ randjs/test63.js
@@ -1,5 +1,5 @@
 var imgnumber = Math.floor(Math.random()*11);
-var imgurl = ['./海邊/26.jpg', './海邊/10.jpg', './海邊/28.jpg', './海邊/29.jpg', './海邊/30.jpg', './海邊/05.jpg',
+var imgurl = ['./海邊/26.JPG', './海邊/10.jpg', './海邊/28.jpg', './海邊/29.jpg', './海邊/30.jpg', './海邊/05.jpg',
 			  './海邊/25.jpg', './海邊/33.jpg', './海邊/34.jpg', './海邊/03.jpg', './海邊/36.jpg'];
 var imgurlind = ['./海邊html/YinyangSea.html', './海邊html/Fenniaolin.html', './海邊html/HsinchuHaishanFishingPort.html', './海邊html/ChiayiDongshihFishingPort.html', './海邊html/ChanghuaWangongFishingPort.html', './海邊html/Gaomei.html',
 				 './海邊html/JinshanSecretBeach.html', './海邊html/PenghuQimeiTwin-HeartsStoneWeir.html', './海邊html/KentingBeach.html', './海邊html/NeipiBeach.html', './海邊html/KentingSouthBeach.html']
