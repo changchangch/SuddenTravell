e7546f177260a00785408ec2552b3bc158554de4
changchangch <45754721+changchangch@users.noreply.github.com>
2019-05-01 01:19:07 +0800

    Delete test33.js

--- randjs/test33.js
+++
@@ -1,4 +0,0 @@
-var imgnumber = Math.floor(Math.random()*3);
-var imgurl = ['./花海/縮小/19.jpg', './花海/縮小/20.jpg', './花海/縮小/21.jpg'];
-var imgurlind = ['./花海html/PingtungXinpi.html', './花海html/MiaoliTongluo.html', './花海html/TaShee.html']
-document.write('<a href="'+imgurlind[imgnumber]+'" target="_blank">'+'<img src="'+imgurl[imgnumber]+'">'+'</a>');
