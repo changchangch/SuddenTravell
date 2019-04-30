9e2f94142c73cdc9edbfae4e4f8933dc5fd8c9a9
changchangch <45754721+changchangch@users.noreply.github.com>
2019-05-01 01:20:03 +0800

    Delete test42.js

--- randjs/test42.js
+++
@@ -1,4 +0,0 @@
-var imgnumber = Math.floor(Math.random()*3);
-var imgurl = ['./人造建築物/縮小/10.jpg', './人造建築物/縮小/11.jpg', './人造建築物/縮小/12.jpg'];
-var imgurlind = ['./人造建築物html/Taipei101.html', './人造建築物html/Taipei101.html', './人造建築物html/SongshanCulturalandCreativePark.html']
-document.write('<a href="'+imgurlind[imgnumber]+'" target="_blank">'+'<img src="'+imgurl[imgnumber]+'" style="max-width: 95%; margin: auto;">'+'</a>');
