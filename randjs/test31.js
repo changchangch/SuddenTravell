8616b7b957774f42615c8cc79a1111a5e6d72ada
changchangch <45754721+changchangch@users.noreply.github.com>
2019-05-01 01:18:54 +0800

    Delete test31.js

--- randjs/test31.js
+++
@@ -1,4 +0,0 @@
-var imgnumber = Math.floor(Math.random()*3);
-var imgurl = ['./花海/縮小/01.jpg', './花海/縮小/02.jpg', './花海/縮小/03.jpg'];
-var imgurlind = ['./花海html/wulingfarm.html', './花海html/wulingfarm.html', './花海html/wulingfarm.html']
-document.write('<a href="'+imgurlind[imgnumber]+'" target="_blank">'+'<img src="'+imgurl[imgnumber]+'">'+'</a>');
