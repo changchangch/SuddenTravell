86995d1e6387a36b9a7b2c25933a51af699cc359
changchangch <45754721+changchangch@users.noreply.github.com>
2019-05-01 01:17:16 +0800

    Delete test13.js

--- randjs/test13.js
+++
@@ -1,4 +0,0 @@
-var imgnumber = Math.floor(Math.random()*3);
-var imgurl = ['./山/縮小/19.jpg', './山/縮小/20.jpg', './山/縮小/21.jpg'];
-var imgurlind = ['./山html/AOWANDA.html', './山html/Huoyanshan.html', './山html/zoglan.html']
-document.write('<a href="'+imgurlind[imgnumber]+'" target="_blank">'+'<img src="'+imgurl[imgnumber]+'">'+'</a>');
