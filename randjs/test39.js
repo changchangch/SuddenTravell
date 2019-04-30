38290398df4f5ae7460c1f5113b714c4b585fec1
changchangch <45754721+changchangch@users.noreply.github.com>
2019-05-01 01:19:47 +0800

    Delete test39.js

--- randjs/test39.js
+++
@@ -1,4 +0,0 @@
-var imgnumber = Math.floor(Math.random()*3);
-var imgurl = ['./花海/縮小/25.jpg', './花海/縮小/26.jpg', './花海/縮小/27.jpg'];
-var imgurlind = ['./花海html/ChanghuaErlin.html', './花海html/ChanghuaBeidou.html', './花海html/HsinchuXiuhu.html']
-document.write('<a href="'+imgurlind[imgnumber]+'" target="_blank">'+'<img src="'+imgurl[imgnumber]+'">'+'</a>');
