c8f32ce4e45a44346d7feff02cc6257a1ef78039
changchangch <45754721+changchangch@users.noreply.github.com>
2019-05-01 01:19:00 +0800

    Delete test32.js

--- randjs/test32.js
+++
@@ -1,4 +0,0 @@
-var imgnumber = Math.floor(Math.random()*3);
-var imgurl = ['./花海/縮小/10.jpg', './花海/縮小/11.jpg', './花海/縮小/12.jpg'];
-var imgurlind = ['./花海html/FushoushanFarm.html', './花海html/ShilinResidence.html', './花海html/ShilinResidence.html']
-document.write('<a href="'+imgurlind[imgnumber]+'" target="_blank">'+'<img src="'+imgurl[imgnumber]+'">'+'</a>');
